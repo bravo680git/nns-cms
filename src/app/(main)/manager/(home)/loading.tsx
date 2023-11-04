5d9ce8dddca33e031b2180d3d242b2dac52d9786
iambravo680 <iambravo680@gmail.com>
2023-11-04 19:59:42 +0700

    create dashboard loading

--- src/app/(main)/manager/(home)/loading.tsx
+++ src/app/(main)/manager/(home)/loading.tsx
@@ -1,5 +1,34 @@
+"use client";
+
+import { Col, Row, Skeleton } from "antd";
+
 function Loading() {
-    return <div data-component="Loading"></div>;
+    return (
+        <div data-component="Loading">
+            <Skeleton active paragraph={{ rows: 0 }} />
+            <Row wrap gutter={[16, 16]}>
+                <Col flex="240px">
+                    <Skeleton.Input active block style={{ height: 100 }} />
+                </Col>
+                <Col flex="240px">
+                    <Skeleton.Input active block style={{ height: 100 }} />
+                </Col>
+            </Row>
+            <Skeleton
+                active
+                paragraph={{ rows: 0 }}
+                style={{ marginTop: 16 }}
+            />
+            <Row gutter={[16, 16]}>
+                <Col span={24} lg={10}>
+                    <Skeleton.Input active block style={{ height: 400 }} />
+                </Col>
+                <Col span={24} lg={14}>
+                    <Skeleton.Input active block style={{ height: 400 }} />
+                </Col>
+            </Row>
+        </div>
+    );
 }
 
 export default Loading;
